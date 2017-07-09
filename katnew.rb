f558632b41c546a91aaa51bc5d3a07ffe84e5811
misturfixit <japullins420@gmail.com>
2017-07-09 19:04:03 -0400

    fail

--- katnew.rb
+++ katnew.rb
@@ -4,12 +4,21 @@ def new_kat
 	arry = []
 
 	1.upto(100) do |n|
-		if (n %5 == 0) && (n %3 == 0)
-		arry << "MinedMinds"	
 		
-		else arry << n
+		if (n %5 == 0) && (n %3 == 0)
+			arry << "MinedMinds"	
+		
+		elsif (n %3 == 0)
 
-	   end
+			arry << "Mined"	
+		
+
+
+		else 
+			arry << n
+		end
+
+
 	end
 	
 	return arry
--- testnew.rb
+++ testnew.rb
@@ -12,7 +12,13 @@ class TestArr < Minitest::Test
 	def test_pos0
 		    assert_equal(1, new_kat[0])	
 	end	
+	
+	def test_pos2
+			 assert_equal("Mined", new_kat[2]) 
+	end
 
-
-
+	def test_pos4 
+			assert_equal( "Minds", new_kat[4])
+	end
+	
 end 
